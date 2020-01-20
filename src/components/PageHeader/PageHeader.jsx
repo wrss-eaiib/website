6910ce5004fd77e660576d3bdfe33e82145f9cca
s4ilor <pawel.olbrycht.pl@gmail.com>
2020-01-20 15:41:53 +0100

    first commit

---
+++ src/components/PageHeader/PageHeader.jsx
@@ -0,0 +1,46 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+
+class PageHeader extends React.Component {
+  render() {
+    return (
+      <div className="page-header header-filter">
+          <img src="~/Downloads/wallpapers/5t9JIKR.jpg"></img>
+        <div className="squares square1" />
+        <div className="squares square2" />
+        <div className="squares square3" />
+        <div className="squares square4" />
+        <div className="squares square5" />
+        <div className="squares square6" />
+        <div className="squares square7" />
+        <Container>
+          <div className="content-center brand">
+            <h1 className="h1-seo">WRSS Wydziału Elektrotechniki, Automatyki, Informatyki i Inżynierii Biomedycznej</h1>
+            <h3 className="d-none d-sm-block">AGH Krakow</h3>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default PageHeader;
